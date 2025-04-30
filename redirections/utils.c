9ce04d046286ed9f8e6cd39ee9fc15c19b9c3216
ayman elfadl <aymanelfadl16@gmail.com>
2025-04-30 20:22:57 +0100

    ok

--- redirections/utils.c
+++ redirections/utils.c
@@ -17,3 +17,21 @@ int wait_for_child(pid_t child_pid)
         return 128 + WTERMSIG(status);
     return 1;
 }
+
+char *ft_strncpy(char *dest, const char *src, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return (dest);
+}
